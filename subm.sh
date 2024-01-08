ec240fa93718cd787a7f1ff93833287d09a0c25b
Megan O'Hara <megan.ohara@bangor.ac.uk>
2024-01-08 13:38:45 +0000

    Add files via upload

---
+++ subm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#job name
+#SBATCH --job-name=n_r_xdim
+#SBATCH --output=results_1
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=10
+#SBATCH --partition=compute
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=osu894@bangor.ac.uk
+
+#normal batch commands
+module load mpi/intel/2020/4
+
+mpirun ./mitgcmuv > output.dat
